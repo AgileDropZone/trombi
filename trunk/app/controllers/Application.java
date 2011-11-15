5c13b658f2fb16a454f370ddbb001984676436b1
contact@unprojetdeboutenbout.fr <contact@unprojetdeboutenbout.fr@0dfbc4b0-c6c0-83ae-1a5a-2c650e5c856b>
2011-11-15 12:20:21 +0000

    Première page d'accueil et de la page de création/modification d'un profil

--- trunk/app/controllers/Application.java
+++ trunk/app/controllers/Application.java
@@ -1,16 +1,72 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.*;
-
-import models.*;
+import models.Identite;
+import models.PaginationVerticale;
+import play.db.jpa.Model;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		// Fixtures.deleteDatabase();
+		List<Identite> lstIdentite = Identite.findAll();
+		if (lstIdentite.isEmpty()) {
+
+			// Création d'un jeu de donnée initial
+			Identite id = new Identite("MARTIN", "Henry");
+			id.save();
+			lstIdentite.add(id);
+			id = new Identite("DUPOND", "Robert");
+			id.save();
+			lstIdentite.add(id);
+			id = new Identite("DUPONT", "Marc");
+			id.save();
+			lstIdentite.add(id);
+
+			for (int i = 0; i < 30; i++) {
+				Identite idG = new Identite("ID" + i, "Prenom" + i);
+				idG.save();
+				lstIdentite.add(idG);
+			}
+		}
+
+		List<PaginationVerticale> lstPaginee = Application.paginer(lstIdentite,
+				5);
+		render(lstPaginee);
+	}
+
+	public static List<PaginationVerticale> paginer(List<Identite> lstAPaginer,
+			int nbColonnes) {
+		List<PaginationVerticale> lstPaginee = new ArrayList<PaginationVerticale>();
+		int numCol = 0;
+		PaginationVerticale elementPagine = new PaginationVerticale();
+		for (Model element : lstAPaginer) {
+			if (numCol == 0) {
+				elementPagine.col1 = element;
+			}
+			if (numCol == 1) {
+				elementPagine.col2 = element;
+			}
+			if (numCol == 2) {
+				elementPagine.col3 = element;
+			}
+			if (numCol == 3) {
+				elementPagine.col4 = element;
+			}
+			if (numCol == 4) {
+				elementPagine.col5 = element;
+				lstPaginee.add(elementPagine);
+				elementPagine = new PaginationVerticale();
+			}
+			numCol = (numCol + 1) % nbColonnes;
+		}
+		if (numCol != 0)
+			lstPaginee.add(elementPagine);
+
+		return lstPaginee;
+	}
 
 }
