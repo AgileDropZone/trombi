7bafeec9ae7e550517f51e7e0efd2fe9edc904a9
contact <contact@unprojetdeboutenbout.fr>
2011-11-29 20:03:35 +0000

    Première pagination. Ne concerne pour le moment *que* la page d'accueil. -> Des optimisations et mises au points sont encore nécessaires. -> Il faut appliquer la pagination *aussi* aux résultats de recherche

--- test/controllers/ApplicationTest.java
+++ test/controllers/ApplicationTest.java
@@ -37,5 +37,53 @@ public class ApplicationTest extends FunctionalTest {
 		assertIsOk(response);
 		assertContentType("text/html", response);
 		assertContentMatch("Robert DUPOND", response);
+
+		String strReponse = getContent(response);
+		assertTrue("Henry MARTIN ne devrait pas apparaitre sur cette page",
+				strReponse.indexOf("Henry MARTIN") == -1);
+
 	}
+
+	@Test
+	public void laPageAccueilNeDoitAfficherQue15Profils() {
+		Response response = GET("/");
+		assertIsOk(response);
+		assertContentType("text/html", response);
+
+		// Vis-à-vis du jeu de données de test, nous devons avoir
+		// sur la première page de Céline Ampère à Carine Lavoisier
+		// Les profils de Henry MARTIN à Naomie WATT doivent
+		// apparaitre en page 2
+
+		assertContentMatch("C&eacute;line AMP&Egrave;RE", response);
+		assertContentMatch("Carine LAVOISIER", response);
+
+		String strReponse = getContent(response);
+		assertTrue("Henry MARTIN ne devrait pas apparaitre sur cette page",
+				strReponse.indexOf("Henry MARTIN") == -1);
+		assertTrue("Naomie WATT ne devrait pas apparaitre sur cette page",
+				strReponse.indexOf("Naomie WATT") == -1);
+	}
+
+	@Test
+	public void laSecondePageDoitAfficherLesProfilsDe16a30() {
+		Response response = GET("/page2");
+		assertIsOk(response);
+		assertContentType("text/html", response);
+
+		// Vis-à-vis du jeu de données de test, nous devons avoir
+		// sur la seconde page de Henry MARTIN à Naomie WATT
+		// Les profils de Céline Ampère à Carine Lavoisier doivent
+		// apparaitre en page 1
+
+		assertContentMatch("Henry MARTIN", response);
+		assertContentMatch("Naomie WATT", response);
+
+		String strReponse = getContent(response);
+		assertTrue("Céline AMPÈRE ne devrait pas apparaitre sur cette page",
+				strReponse.indexOf("C&eacute;line AMP&Egrave;RE") == -1);
+		assertTrue("Carine LAVOISIER ne devrait pas apparaitre sur cette page",
+				strReponse.indexOf("Carine LAVOISIER") == -1);
+	}
+
 }
